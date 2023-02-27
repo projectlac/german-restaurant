77440d5740648bb343632ec9d8e6be096faaf64c
Tim Dang <dft1711198@gmail.com>
2023-02-27 10:32:15 +0700

    huhu

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,29 +1,82 @@
 import styles from "@/styles/Home.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactComponent as MySvgFile } from "@/assets/svgs/menu.svg";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <section className="flex">
-      <nav className={styles.navigator}>
-        <div className={styles.logo}>LOGO</div>
-        <ul>
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href={"/menu"}>Gallery</Link>
-          </li>
-        </ul>
+  const route = useRouter();
 
-        <h5>Adress</h5>
-        <p>
-          Klarastraße 27 <br />
-          45130 Essen <br />
-          0201 4588 1209
-        </p>
+  return (
+    <section className="flex flex-col md:flex-row">
+      <nav className={`${styles.navigator}`}>
+        <div className="pc  md:block hidden">
+          <div className={styles.logo}>LOGO</div>
+          <ul>
+            <li
+              className={route.asPath === "/" ? styles["navigator-active"] : ""}
+            >
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li
+              className={
+                route.asPath === "/gallery" ? styles["navigator-active"] : ""
+              }
+            >
+              <Link href={"/gallery"}>Gallery</Link>
+            </li>
+          </ul>
+
+          <h5>Adress</h5>
+          <p>
+            Klarastraße 27 <br />
+            45130 Essen <br />
+            0201 4588 1209
+          </p>
+        </div>
+        <div className="mobile md:hidden block">
+          <div className="flex justify-between">
+            <span className="w-1/4"></span>
+            <span className={styles.logo}>LOGO</span>
+            <span
+              className={`${styles["menu-button"]} w-1/4 flex align-center justify-end`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div className={styles["menu-mobile"]}>
+          <ul>
+            <li
+              className={route.asPath === "/" ? styles["navigator-active"] : ""}
+            >
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li
+              className={
+                route.asPath === "/gallery" ? styles["navigator-active"] : ""
+              }
+            >
+              <Link href={"/gallery"}>Gallery</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       <div className={styles.main}>{children}</div>
