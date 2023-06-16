6b506ad59e011152c1344de4ba3f7a4cfecd1033
projectlac <dft1711198@gmail.com>
2023-06-16 19:55:32 +0700

    huhu

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,4 +1,4 @@
-import image from "./../../../public/npho.JPG";
+import image from "@/assets/npho.JPG";
 import { useAppContext } from "@/contexts/state";
 import styles from "@/styles/Home.module.scss";
 import Image from "next/image";
