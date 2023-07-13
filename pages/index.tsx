00b67fe95942323812898f95761d382aa4730fd2
Sotatek-TuyenDang2 <tuyen.dang2@sotatek.com>
2023-07-13 14:44:56 +0700

    Change

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Menu() {
         <meta name="description" content="Menu" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/thirteen.svg" /> */}
-        <meta property="og:image" content="./public/ogImage.jpg" />
+        <meta property="og:image" content="./ogImage.jpg" />
         <meta property="og:title" content="ĐisĂnĐi Restaurant" />
         <meta property="og:description" content="ĐisĂnĐi Restaurant - Vietnamese Restaurant & Bar" />
    
